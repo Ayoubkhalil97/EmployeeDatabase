4fddc58a1f924c554d0c663a24707741891e899f
Ayoubkhalil97 <100157383+Ayoubkhalil97@users.noreply.github.com>
2023-02-14 15:08:52 +0000

    Add files via upload

---
+++ EmployeeService.java
@@ -0,0 +1,21 @@
+package net.javaguides.springboot.service;
+
+import java.util.List;
+
+import net.javaguides.springboot.model.Employee;
+
+public interface EmployeeService {
+	
+	Employee saveEmployee(Employee employee);
+	
+	List<Employee> getAllEmployees();
+
+	Employee  getEmployeeById(long id);
+	
+	Employee updateEmployee(Employee employee, long Id);
+	
+	void  deleteEmployee(long id);
+	
+}
+
+	
